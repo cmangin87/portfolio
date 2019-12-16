4e8065787751d0d25a212496005a3bbe270c5759
Christopher Mangin <mangin.christopher@gmail.com>
2019-12-16 10:18:13 -0500

    Updates

---
+++ precache-manifest.91b1367082cab68c8462fadc00c33c21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "4198736726bad1c1e8fa",
+    "url": "/portfolio/static/js/main.41987367.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "4198736726bad1c1e8fa",
+    "url": "/portfolio/static/css/main.a2f00f97.chunk.css"
+  },
+  {
+    "revision": "908857e9d2fab98ea8c5720c80663338",
+    "url": "/portfolio/index.html"
+  }
+];
