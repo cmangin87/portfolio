dc1bdce349fa670b7ac264ed091bddab3414143f
Christopher Mangin <49618564+cmangin87@users.noreply.github.com>
2019-12-19 12:38:58 -0500

    Add files via upload

---
+++ resumeData.js
@@ -0,0 +1,128 @@
+let resumeData = {
+    "name": "Christopher R. Mangin",
+    "role": "Certified Full-Stack Web Developer",
+    "roleDescription": "HTML/CSS, JavaScript, jQuery, Node, Git, MySQL,", 
+    "roleDescription2": "MongoDB, Express, React, and more",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/christopher-mangin-75a7021a/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/cmangin87",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"Consistent and dependable collaborator with 10 years of experience in the social services sector excited to embark on a new career in full stack web development.",
+    "moreaboutme":"Independent self-starter with a passion for creative problem solving and the ability to balance a complex workload while managing and motivating a team.",
+    "address":"301 Branch Drive",
+    "address2": "Branchburg, NJ, 08853",
+    "phone": "(201) 306-3748",
+    "email": "mangin.christopher@gmail.com",
+
+    "education":[
+      {
+        "UniversityName":"Rutgers Coding Bootcamp",
+        "specialization":"Web Development",
+        "specialization2":"General Programming",
+        "MonthOfPassing":"Dec ",
+        "YearOfPassing":"2019",
+        "Achievements":"Certification: "
+      },
+      {
+        "UniversityName":"Udemy",
+        "specialization":"The Complete JavaScript Course",
+        "specialization2":"An Introduction to Python Programming",
+        "MonthOfPassing":"Dec ",
+        "YearOfPassing":"2019",
+        "Achievements":"Certification: "
+      },
+      {
+        "UniversityName":"University of Delaware",
+        "specialization":"Psychology (Major)",
+        "specialization2":"Philosophy (Minor)",
+        "MonthOfPassing":"Jan ",
+        "YearOfPassing":"2009",
+        "Achievements":"Bachelor of Arts Degree: "
+      }
+    ],
+    "work":[
+      
+      // {
+      //   "CompanyName":"Comprehensive Behavioral Healthcare ",
+      //   "specialization":"Managed a complex workload in collaboration with a team of 25 to ensure participant engagement and safety",
+      //   "MonthOfLeaving":" April",
+      //   "YearOfLeaving":"2017",
+      //   "Achievements":""
+      // },
+      // {
+      //   "CompanyName":"Comprehensive Behavioral Healthcare ",
+      //   "specialization":"Independent off-site liaison for family of mentally ill patients including weekly facilitation of support groups",
+      //   "MonthOfLeaving":" July",
+      //   "YearOfLeaving":"2014",
+      //   "Achievements":""
+      // },
+      {
+        "CompanyName":"Our House, Inc. ",
+        "CompanyName2":"Comprehensive Behavioral Healthcare ",
+        "CompanyName3":"Community Hope ",
+        "specialization1": " Willing to take on challenging work and driven to accomplish goals",
+        "specialization2": " Independently motivated and quick to comprehend",
+        "specialization3": " Team player with experience navigating ambiguous and complex work environments",
+        "specialization4": " Experienced in leading others and delegating tasks amongst a team",
+        "specialization5": " Clear communicator and active listener",
+        "specialization6": " Trained crisis manager and problem solver",
+        // "specialization7": " ",
+        // "specialization8": " ",
+        "MonthOfLeaving":" Aug 2018 - Sept 2019 ",
+        "MonthOfLeaving2":" Oct 2012 - Aug 2018 ",
+        "MonthOfLeaving3":" Feb 2009 - Aug 2015 ",
+        "YearOfLeaving":"",
+        "YearOfLeaving2":"",
+        "YearOfLeaving3":"",
+        "Achievements":""
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"Git Clash",
+        "description":"An innovative way to simplify the hiring process.. or to simply challenge your colleagues!  Also, a one stop shop for searching GitHub usernames for all relevant information.",
+        "imgurl":"images/portfolio/gitclashbutton.png",
+        "url":"http://git-clash.herokuapp.com/"
+      },
+      {
+        "name":"Dino in Space",
+        "description":"A side-scroller game where the player controls a dinosaur lost in space after a meteor crashed into Earth. Traverse through space by keeping the dinosaur suspended in the air using the arrow keys as he avoids obstacles in his path.",
+        "imgurl":"images/portfolio/dinoinspacebutton.png",
+        "url":"https://secure-lowlands-84653.herokuapp.com/"
+      },
+      {
+        "name":"Treat Yo Self",
+        "description":"Need a low-key date night idea but feeling indecisive?  Let us decide for you!  Choose your preferred cuisine and movie genre and we will generate a fitting combination just for you.",  
+        "imgurl":"images/portfolio/treatyoselfbutton.png",
+        "url":"https://jacobwilder.github.io/Project-1/"
+      },
+      // {
+      //   "name":"Crystals Collector",
+      //   "description":"Compile your crystals in such a way that they add up to your target score.",
+      //   "imgurl":"images/portfolio/crystalgame.png",
+      //   "url":"https://cmangin87.github.io/unit4-crystals/"
+      // },
+      // {
+      //   "name":"Simpsons Clicky",
+      //   "description":"Try to click every Simpsons character consecutively without repeating yourself!",
+      //   "imgurl":"images/portfolio/simpsonsclicky.png",
+      //   "url":"https://simpsons-clicky-memory-game.herokuapp.com/"
+      // },
+      // {
+      //   "name":"45 Second Trivia",
+      //   "description":"Can you answer 8 trivia questions in 45 seconds?",
+      //   "imgurl":"images/portfolio/trivia.png",
+      //   "url":"https://cmangin87.github.io/TriviaGame/"
+      // }
+    ]
+  }
+  
+  export default resumeData
