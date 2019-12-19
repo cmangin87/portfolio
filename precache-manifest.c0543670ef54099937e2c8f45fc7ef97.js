d79d5cb7b886f4bcf0eca4205409a2dff5c4fb9a
Christopher Mangin <mangin.christopher@gmail.com>
2019-12-19 12:32:05 -0500

    Updates

--- precache-manifest.c0543670ef54099937e2c8f45fc7ef97.js
+++ precache-manifest.c0543670ef54099937e2c8f45fc7ef97.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "4198736726bad1c1e8fa",
-    "url": "/portfolio/static/js/main.41987367.chunk.js"
+    "revision": "6dbca3d1583267f2864f",
+    "url": "/portfolio/static/js/main.6dbca3d1.chunk.js"
   },
   {
     "revision": "6eb6405c9d99258624bc",
     "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
   },
   {
-    "revision": "4198736726bad1c1e8fa",
+    "revision": "6dbca3d1583267f2864f",
     "url": "/portfolio/static/css/main.a2f00f97.chunk.css"
   },
   {
-    "revision": "908857e9d2fab98ea8c5720c80663338",
+    "revision": "6544d83b116ddb2c97f57ad6706ef3f8",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.91b1367082cab68c8462fadc00c33c21.js"
+  "/portfolio/precache-manifest.c0543670ef54099937e2c8f45fc7ef97.js"
 );
 
 workbox.clientsClaim();
